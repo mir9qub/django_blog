2e14a3e53ee0e12513b303bd89b36671464c3f2d
Salamat <nsalamat18@gmail.com>
2022-02-08 15:35:34 +0500

    Added django-heroku

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,5 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
