a2e9754bc4a690c1ad398b4f5d998473011309f4
Salamat <nsalamat18@gmail.com>
2022-02-09 00:51:10 +0500

    modified again users/models

--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super(Profile, self).save(*args, **kwargs)
+    def save(self, **kwargs):
+        super.save()
 
         img = Image.open(self.image.path)
 
