a9c3873fc32ac974b79060186470693959a07586
Salamat <nsalamat18@gmail.com>
2022-02-08 15:02:33 +0500

    Updated Settings

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)%wc=52p4n#3w0+yzymfmi8^x77#aii5)vrq+8clkx)xis3+3'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myawesomedjangoappn.herokuapp.com']
 
 
 # Application definition
