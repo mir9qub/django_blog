9bda14510007dadc6d462100a8b624b37fda2135
Salamat <nsalamat18@gmail.com>
2022-02-10 00:52:00 +0500

    modified users/views

--- users/views.py
+++ users/views.py
@@ -18,6 +18,7 @@ def register(request):
 
 @login_required
 def profile(request):
+    Profile.objects.get_or_create(user=request.user)
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
