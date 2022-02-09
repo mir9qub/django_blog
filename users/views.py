ed1535c27bece4e3be51c324a4bd2fdd4c57d945
Salamat <nsalamat18@gmail.com>
2022-02-10 00:55:55 +0500

    modified users/views

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-
+from .models import Profile
 
 def register(request):
     if request.method == "POST":
