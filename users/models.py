628caff1d8f4e75a76c1f9c3366654cbbbba81e6
Salamat <nsalamat18@gmail.com>
2022-02-10 11:31:09 +0500

    modified users/models

--- users/models.py
+++ users/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
 
 
         #img = Image.open(self.image.path)
-
+        img = Image.open(storage.open(self.image.name))
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
